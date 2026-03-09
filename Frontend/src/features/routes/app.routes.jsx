b06a019c0a561fe1b66d53e617e4c6b503004afc
Sujal Panchal <Sujalpanchal249@gmail.com>
2026-03-09 22:14:33 +0530

    Add Home page layout and styling, include Movie component, and update index.html

--- Frontend/src/features/routes/app.routes.jsx
+++ Frontend/src/features/routes/app.routes.jsx
@@ -3,6 +3,7 @@ import Login from "../auth/Pages/login.jsx";
 import Register from "../auth/Pages/register.jsx";
 import Home from "../../features/movies/pages/Home.jsx";
 import Protected from "../auth/components/Protect.jsx";
+import Movie from "../movies/pages/movie.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +19,7 @@ export const router = createBrowserRouter([
     element: (
       <Protected>
         <Home />
+        <Movie />
       </Protected>
     ),
   },
